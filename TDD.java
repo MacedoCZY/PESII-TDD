420a3ff5a99566e4f7f7eda1ecf6766ac4cb33c9
Macedo <gustavomacedo1366@hotmail.com>
2023-07-25 20:53:37 -0300

    tes 2
    alterações na classe dollar
    agora temos um construtor para o dollar
    o qual recebe uma quantia int
    e a função time agora faz:
    quantia = quantia * multiplier
    anteriormente função não fazia nada
    portanto agora irá funcionar

--- TDD.java
+++ TDD.java
@@ -4,15 +4,19 @@ public class TDD{
 	}
 	
 	class Dollar {
-       int amount = 10;
-       Dollar(int amount) {}			
-       void times(int multiplier) {}
+		int amount;
+		Dollar(int amount) {
+		  this.amount= amount;
+		}
+		void times(int multiplier) {
+		  amount= amount * multiplier;
+		}
     }	
 
     public void testMultiplication() {
-       Dollar five = new Dollar(5);
-       five.times(2);
-       assertEquals(10, five.amount);
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
     }
 	
 }

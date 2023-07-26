4555700572dc0dc90cd502d6669e188968c9fd77
Macedo <gustavomacedo1366@hotmail.com>
2023-07-25 21:13:20 -0300

    1 e 2
    a princípio é introduzido uma nova classe para moeda Franc, a  qual contém todos os métodos anteriores do dollar, porém agora dentro da classe
    tanto como, temos também uma função para testar a igualdade das multiplicações dos Franc

--- TDD.java
+++ TDD.java
@@ -14,6 +14,20 @@ public class TDD{
 		}
     }	
 
+	class Franc {   
+		private int amount;					
+			Franc(int amount) {      
+			this.amount= amount;
+		}					
+			Franc times(int multiplier)  {      
+			return new Franc(amount * multiplier);					
+		}   
+		public boolean equals(Object object) {					
+			Franc franc = (Franc) object;      
+			return amount == franc.amount;					
+		}					
+	}
+
     public void testMultiplication() {
 		Dollar five = new Dollar(5);
 		assertEquals(new Dollar(10), five.times(2));
@@ -28,5 +42,11 @@ public class TDD{
 	public boolean equals(Object object)  {
 		Dollar dollar = (Dollar) object;
 		return amount == dollar.amount;
+	}
+	
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
 	}
 }
